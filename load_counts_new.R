75837d85e70e5a18337a966430c7f71a4d633268
Khalif Hussein <a23khahu@student.his.se>
2025-12-16 23:49:49 +0100

    Keep only Rcode package; remove root files

--- load_counts_new.R
+++
@@ -1,6 +0,0 @@
-load_counts <- function(counts_path) {
-  x <- read.table(counts_path, header = TRUE, sep = "\t", check.names = FALSE)
-  rownames(x) <- x$Gene
-  x$Gene <- NULL
-  as.matrix(x)
-}
--- load_samples_new.R
+++
@@ -1,5 +0,0 @@
-load_samples <- function(samples_path) {
-  s <- read.table(samples_path, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-  s$disease <- factor(s$disease)
-  s
-}
